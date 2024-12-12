061e71d69989374baf32bb666abd40755a8e0619
Nidhi <nidhisingh5958@gmail.com>
2024-12-12 13:01:55 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# shop_app
+
+A new Flutter project.
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
---
+++ android/app/src/main/kotlin/com/example/soap_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.soap_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
